6bef1d2a4a0bc7a6bf6838d48405d132309d61ac
Enesince98 <enes98lir@gmail.com>
2022-06-12 22:59:42 +0300

    Added tons of updates sorry for not creating a new branch

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import Table from './Table'
 
 const Contents = () => {
   return (
-    <div>Contents</div>
+    <div><Table url = 'https://62a492ef47e6e40063951ec5.mockapi.io/api/contents'/></div>
   )
 }
 
